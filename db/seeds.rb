ae9d4d076423c4efc8ac8961377db5e9bcaa0df5
Corinne <cfb8484@gmail.com>
2014-02-02 17:01:52 +1100

    second post

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,6 @@ Comment.destroy_all
 u1 = User.create(:name => "Corinne", :password => "password", :password_confirmation => "password")
 
 p1 = Post.create(:title => "First Post", :body => "Hello World!")
+p2 = Post.create(:title => "Second Post", :body => "Hello Again")
 
 u1.posts << p1
